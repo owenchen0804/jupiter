b8bbec9f26a370dffa451a30ed22f38ec7369269
owenchen0804 <owenchen0804@gmail.com>
2021-06-05 08:51:43 -0700

    Adding SearchServlet, and adding methods to TwitchClient class

---
+++ src/main/java/com/laioffer/jupiter/entity/Item.java
@@ -0,0 +1,157 @@
+package com.laioffer.jupiter.entity;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonDeserialize(builder = Item.Builder.class)
+public class Item {
+    @JsonProperty("id")
+    private final String id;    // final 是因为返回的数据有这些信息 且我们不想修改这些信息
+
+    @JsonProperty("title")
+    private final String title;
+
+    @JsonProperty("thumbnail_url")
+    private final String thumbnailUrl;
+
+    // @JsonAlias indicates that the field could be retrieved by another key.
+    // 两个key map到同一个 property, clip 里用的是  "broadcaster_name"，
+    // video 和 stream 用的是 "user_name"
+    @JsonProperty("broadcaster_name")
+    @JsonAlias({ "user_name" })
+    private String broadcasterName;
+
+    @JsonProperty("url")
+    private String url; // 返回的数据可能不带 url 需要我们自己 set
+
+    @JsonProperty("game_id")
+    private String gameId;  // 返回的数据可能不带 gameId 需要我们自己 set
+
+    @JsonProperty("item_type")
+    private ItemType type;  // 返回的数据不带 ItemType 是我们自己定义的
+
+    private Item(Builder builder) {
+        this.id = builder.id;
+        this.title = builder.title;
+        this.url = builder.url;
+        this.thumbnailUrl = builder.thumbnailUrl;
+        this.broadcasterName = builder.broadcasterName;
+        this.gameId = builder.gameId;
+        this.type = builder.type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    // Item class has some setters as well because fields like type, url and gameId
+    // may not return from Twitch,
+    // and we need to update the value after Jackson deserialize the data.
+    public Item setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+    public String getBroadcasterName() {
+        return broadcasterName;
+    }
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public Item setGameId(String gameId) {
+        this.gameId = gameId;
+        return this;
+    }
+
+    public ItemType getType() {
+        return type;
+    }
+
+    public Item setType(ItemType type) {
+        this.type = type;
+        return this;
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class Builder {
+        @JsonProperty("id")
+        private String id;
+
+        @JsonProperty("title")
+        private String title;
+
+        @JsonProperty("url")
+        private String url;
+
+        @JsonProperty("thumbnail_url")
+        private String thumbnailUrl;
+
+        @JsonProperty("broadcaster_name")
+        @JsonAlias({ "user_name" })
+        private String broadcasterName;
+
+        @JsonProperty("game_id")
+        private String gameId;
+
+        @JsonProperty("item_type")
+        private ItemType type;
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder url(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public Builder thumbnailUrl(String thumbnailUrl) {
+            this.thumbnailUrl = thumbnailUrl;
+            return this;
+        }
+
+        public Builder broadcasterName(String broadcasterName) {
+            this.broadcasterName = broadcasterName;
+            return this;
+        }
+
+        public Builder gameId(String gameId) {
+            this.gameId = gameId;
+            return this;
+        }
+
+        public Builder type(ItemType type) {
+            this.type = type;
+            return this;
+        }
+
+        public Item build() {
+            return new Item(this);
+        }
+    }
+}
--- src/main/java/com/laioffer/jupiter/external/TwitchClient.java
+++ src/main/java/com/laioffer/jupiter/external/TwitchClient.java
@@ -3,6 +3,8 @@ package com.laioffer.jupiter.external;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import com.laioffer.jupiter.entity.Item;
+import com.laioffer.jupiter.entity.ItemType;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
@@ -15,8 +17,8 @@ import java.io.IOException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.laioffer.jupiter.entity.Game;
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.*;
 
 
 public class TwitchClient {
@@ -25,6 +27,12 @@ public class TwitchClient {
     private static final String TOP_GAME_URL = "https://api.twitch.tv/helix/games/top?first=%s";
     private static final String GAME_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/games?name=%s";
     private static final int DEFAULT_GAME_LIMIT = 20;
+    private static final String STREAM_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/streams?game_id=%s&first=%s";
+    private static final String VIDEO_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/videos?game_id=%s&first=%s";
+    private static final String CLIP_SEARCH_URL_TEMPLATE = "https://api.twitch.tv/helix/clips?game_id=%s&first=%s";
+    private static final String TWITCH_BASE_URL = "https://www.twitch.tv/";
+    private static final int DEFAULT_SEARCH_LIMIT = 20;
+
 
     // Build the request URL which will be used when calling Twitch APIs,
     // e.g. https://api.twitch.tv/helix/games/top when trying to get top games.
@@ -48,10 +56,26 @@ public class TwitchClient {
         }
     }
 
+    // Similar to buildGameURL, build Search URL that will be used when calling Twitch API.
+    // e.g. https://api.twitch.tv/helix/clips?game_id=12924.
+    private String buildSearchURL(String url, String gameId, int limit) {
+        try {
+            gameId = URLEncoder.encode(gameId, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        // 替换占位符
+        // https://api.twitch.tv/helix/streams?game_id=16898&first=20
+        return String.format(url, gameId, limit);
+    }
+
+
     // Send HTTP request to Twitch Backend based on the given URL
     // and returns the body of the HTTP response returned from Twitch backend.
     private String searchTwitch(String url) throws TwitchException {
         // httpclient 用来帮助发送请求
+        // input: response
+        // lambda expression like anonymous class
         CloseableHttpClient httpclient = HttpClients.createDefault();
         // Define the response handler to parse and return HTTP response body returned from Twitch
         ResponseHandler<String> responseHandler = response -> {
@@ -94,7 +118,13 @@ public class TwitchClient {
     private List<Game> getGameList(String data) throws TwitchException {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            return Arrays.asList(mapper.readValue(data, Game[].class));
+            // 把 JSON 格式的array convert 为Java Object
+            // 如果无法一一对应会出现JSONException
+            // 后面一个参数（Game[].class）的意思是 convert 成 Game 这个 class 组成的 array
+            // JSON -> Java Object Game
+            Game[] games = mapper.readValue(data, Game[].class);
+            return Arrays.asList(games);
+            // return Arrays.asList(mapper.readValue(data, Game[].class));
         } catch (JsonProcessingException e) {
             e.printStackTrace();
             throw new TwitchException("Failed to parse game data from Twitch API");
@@ -118,6 +148,85 @@ public class TwitchClient {
         return null;
     }
 
+
+
+    // Similar to getGameList, convert the json data returned from Twitch to a list of Item objects.
+    private List<Item> getItemList(String data) throws TwitchException {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return Arrays.asList(mapper.readValue(data, Item[].class));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new TwitchException("Failed to parse item data from Twitch API");
+        }
+    }
+
+    // Returns the top x streams based on game ID.
+    private List<Item> searchStreams(String gameId, int limit) throws TwitchException {
+        List<Item> streams = getItemList(searchTwitch(buildSearchURL(STREAM_SEARCH_URL_TEMPLATE, gameId, limit)));
+        // Step 1:
+        // String url = buildSearchURL(STREAM_SEARCH_URL_TEMPLATE, gameId, limit);
+        // Step 2:
+        // String data = searchTwitch(url);
+        // Step 3:
+        // List<Item> streams = getItemList(data);
+        for (Item item : streams) {
+            item.setType(ItemType.STREAM);
+            item.setUrl(TWITCH_BASE_URL + item.getBroadcasterName());
+        }
+        return streams;
+    }
+
+    // Returns the top x clips based on game ID.
+    private List<Item> searchClips(String gameId, int limit) throws TwitchException {
+        List<Item> clips = getItemList(searchTwitch(buildSearchURL(CLIP_SEARCH_URL_TEMPLATE, gameId, limit)));
+        for (Item item : clips) {
+            item.setType(ItemType.CLIP);
+        }
+        return clips;
+    }
+
+    // Returns the top x videos based on game ID.
+    private List<Item> searchVideos(String gameId, int limit) throws TwitchException {
+        List<Item> videos = getItemList(searchTwitch(buildSearchURL(VIDEO_SEARCH_URL_TEMPLATE, gameId, limit)));
+        for (Item item : videos) {
+            item.setType(ItemType.VIDEO);
+        }
+        return videos;
+    }
+
+    // 只需调用一个 method  传入不同的参数即可
+    // 不用去 分别调用 三个不同的 method
+    public List<Item> searchByType(String gameId, ItemType type, int limit) throws TwitchException {
+        List<Item> items = Collections.emptyList();
+
+        switch (type) {
+            case STREAM:
+                items = searchStreams(gameId, limit);
+                break;
+            case VIDEO:
+                items = searchVideos(gameId, limit);
+                break;
+            case CLIP:
+                items = searchClips(gameId, limit);
+                break;
+        }
+
+        // Update gameId for all items. GameId is used by recommendation function
+        for (Item item : items) {
+            item.setGameId(gameId); // why seeting gameId gain? 不是input吗？？
+        }
+        return items;
+    }
+
+    // ["Stream" : ... ; "Video" : ... ; "Clip" : ...]
+    public Map<String, List<Item>> searchItems(String gameId) throws TwitchException {
+        Map<String, List<Item>> itemMap = new HashMap<>();
+        for (ItemType type : ItemType.values()) {
+            itemMap.put(type.toString(), searchByType(gameId, type, DEFAULT_SEARCH_LIMIT));
+        }
+        return itemMap;
+    }
 
 
 }
